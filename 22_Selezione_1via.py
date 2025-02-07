19a1ca15840101a16ae1d1f6f410fccdd2f38573
GianoR130 <gianotti.riccardoaldo@itisgrassi.edu.it>
2025-02-07 13:48:03 +0100

    Add files via upload

---
+++ 22_Selezione_1via.py
@@ -0,0 +1,5 @@
+print("Benvenuti nel mio primo codice di conversazione")
+risposta = input("Ti piace andare in bicicletta? ")
+if risposta == "Si":
+    print("Molto bene, ti tieni in forma!")
+print("thefogiscomingthefogiscomingthefogiscomingthefogiscomingthefogiscomingthefogiscomingthefogiscomingthefogiscomingthefogiscomingthefogiscoming")
