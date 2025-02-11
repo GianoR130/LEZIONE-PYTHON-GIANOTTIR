dbd83d4b34d42a058682547c8182b01f9562b91a
GianoR130 <gianotti.riccardoaldo@itisgrassi.edu.it>
2025-02-11 12:02:49 +0100

    Add files via upload

--- 23_Selezione_2vie.py
+++ 23_Selezione_2vie.py
@@ -2,4 +2,6 @@ print("Benvenuti nel mio primo codice di conversazione")
 risposta = input("Ti piace andare in bicicletta? ")
 if risposta == "Si":
     print("Molto bene, ti tieni in forma!")
+else:
+    print("Forse ti piace qualche altro sport.")
 print("Ciao ciao.")
