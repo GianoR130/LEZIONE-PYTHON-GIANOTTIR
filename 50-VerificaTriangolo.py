c530a80f220898f3af9888621475ddba124aa309
GianoR130 <gianotti.riccardoaldo@itisgrassi.edu.it>
2025-03-10 11:53:57 +0100

    Update 50-VerificaTriangolo.py

--- 50-VerificaTriangolo.py
+++ 50-VerificaTriangolo.py
@@ -1,10 +1,9 @@
 lato1 = float(input("Inserire primo lato: "))
 lato2 = float(input("Inserire secondo lato: "))
 lato3 = float(input("Inserire terzo lato: "))
-if lato1 == lato2 or lato2 == lato3:
-    if lato1 == lato3:
-        print("Il triangolo è equilatero")
-    if lato1 != lato3:
-        print("Il triangolo è iscoscele")
-elif lato1 != lato2 or lato2 != lato3:
+if lato1 == lato2 and lato1 == lato3 and lato2 == lato3:
+    print("Il triangolo è equilatero")
+elif lato1 == lato2 or lato1 == lato3 or lato2 == lato3:
     print("Il triangolo è iscoscele")
+elif lato1 != lato2 or lato2 != lato3:
+    print("Il triangolo è scaleno")
